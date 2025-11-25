dab7e4f239a573da08a5f906d71043df75e4cdd9
raihan <raihanizharul7@gmail.com>
2025-11-25 09:56:13 +0700

    revisi tipe data

--- scripts/ddl_bronze.sql
+++ scripts/ddl_bronze.sql
@@ -60,12 +60,12 @@ IF OBJECT_ID('bronze.transaction_db_raw', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.transaction_db_raw (
-    transaction_id      NVARCHAR(50),
-    account_id          NVARCHAR(50),
-    transaction_date    NVARCHAR(50),
-    amount              NVARCHAR(50),
+    transaction_id      INT NOT NULL,
+    account_id          INT,
+    transaction_date    DATETIME2(0),
+    amount              INT,
     transaction_type    NVARCHAR(50),
-    branch_id           NVARCHAR(50)
+    branch_id           INT
 );
 GO
 
@@ -78,11 +78,11 @@ IF OBJECT_ID('bronze.account_db_raw', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.account_db_raw (
-    account_id      NVARCHAR(50),
-    customer_id     NVARCHAR(50),
+    account_id      INT NOT NULL,
+    customer_id     INT,
     account_type    NVARCHAR(50),
-    balance         NVARCHAR(50),
-    date_opened     NVARCHAR(50),
+    balance         INT,
+    date_opened     DATETIME2(0),
     status          NVARCHAR(50)
 );
 GO
@@ -96,10 +96,10 @@ IF OBJECT_ID('bronze.customer_db_raw', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.customer_db_raw (
-    customer_id     NVARCHAR(50),
+    customer_id     INT NOT NULL,
     customer_name   NVARCHAR(50),
     address         NVARCHAR(50),
-    city_id         NVARCHAR(50),
+    city_id         INT,
     age             NVARCHAR(50),
     gender          NVARCHAR(50),
     email           NVARCHAR(50)
@@ -115,7 +115,7 @@ IF OBJECT_ID('bronze.branch_db_raw', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.branch_db_raw (
-    branch_id       NVARCHAR(50),
+    branch_id       INT NOT NULL,
     branch_name     NVARCHAR(50),
     branch_location NVARCHAR(50)
 );
@@ -130,9 +130,9 @@ IF OBJECT_ID('bronze.city_db_raw', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.city_db_raw (
-    city_id     NVARCHAR(50),
+    city_id     INT NOT NULL,
     city_name   NVARCHAR(50),
-    state_id    NVARCHAR(50)
+    state_id    INT
 );
 GO
 
@@ -145,7 +145,7 @@ IF OBJECT_ID('bronze.state_db_raw', 'U') IS NOT NULL
 GO
 
 CREATE TABLE bronze.state_db_raw (
-    state_id     NVARCHAR(50),
+    state_id     INT NOT NULL,
     state_name   NVARCHAR(50)
 );
 GO
